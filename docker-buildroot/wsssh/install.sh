b99e80c440cb4fd68d4c7bc14b6eaa3b82248809
Vincent Lefévère <57573477+vincent-lefevere@users.noreply.github.com>
2026-03-04 18:21:52 +0100

    Update installation method for wsssh
    utilisation de la release 1.0.0

--- docker-buildroot/wsssh/install.sh
+++ docker-buildroot/wsssh/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update
-apt-get install -y --no-install-recommends build-essential ca-certificates git pkg-config
+apt-get install -y --no-install-recommends build-essential ca-certificates wget pkg-config
 apt-get install -y --no-install-recommends libxml2 libxml2-dev libwebsockets-dev libmariadb-dev
-git clone http://github.com/vincent-lefevere/wsssh
-(cd wsssh/src ; make install)
-
+wget https://github.com/vincent-lefevere/wsssh/archive/refs/tags/v1.0.0.tar.gz
+tar -xvzpf v1.0.0.tar.gz
+(cd wsssh-1.0.0/src ; make install)
