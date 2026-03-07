ec237c27551541b5dc7822a70b4fcb10cbea8b78
Vincent Lefévère <57573477+vincent-lefevere@users.noreply.github.com>
2026-03-07 18:57:49 +0100

    correction script d'installation sous Debian13

--- docker-install-debian-13.sh
+++ docker-install-debian-13.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get upgrade
 apt-get -y install ca-certificates curl gnupg lsb-release
 mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
