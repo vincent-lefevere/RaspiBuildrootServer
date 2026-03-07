8d022bc6ea521b9226e7255fd8bf0a4a406d1e4b
Vincent Lefevere <vincent.lefevere@junia.com>
2026-03-07 18:43:06 +0100

    Distinction de la compilation de wsssh selon la version de Debian

--- docker-buildroot/build.sh
+++ docker-buildroot/build.sh
@@ -4,15 +4,24 @@ then
   openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout conf/web/server.key -out conf/web/server.cer
 fi
 mkdir -p bin
-docker container rm tmp_wsssh
-docker rmi make_wsssh:current
-docker build -t make_wsssh:current wsssh
-docker container create --name tmp_wsssh make_wsssh:current
+docker build -t make_wsssh_debian12:current -f wsssh/Dockerfile-Debian12 wsssh
+docker container create --name tmp_wsssh make_wsssh_debian12:current
 docker cp -a -q tmp_wsssh:/usr/local/sbin/wsssh - | tar -C bin -xvf -
 docker container rm tmp_wsssh
-docker rmi make_wsssh:current
-BUILDKIT_PROGRESS=plain docker compose build master-debian12 master-debian13 web mosquitto telegraf mqtt2mysql git || exit
+docker rmi make_wsssh_debian12:current
+BUILDKIT_PROGRESS=plain docker compose build master-debian12 || exit
 rm -Rf bin
+
+mkdir -p bin
+docker build -t make_wsssh_debian13:current -f wsssh/Dockerfile-Debian13 wsssh
+docker container create --name tmp_wsssh make_wsssh_debian13:current
+docker cp -a -q tmp_wsssh:/usr/local/sbin/wsssh - | tar -C bin -xvf -
+docker container rm tmp_wsssh
+docker rmi make_wsssh_debian13:current
+BUILDKIT_PROGRESS=plain docker compose build master-debian13 || exit
+rm -Rf bin
+
+BUILDKIT_PROGRESS=plain docker compose build web mosquitto telegraf mqtt2mysql git || exit
 test -f conf/proftpd/custom/ssh_host_rsa_key && rm conf/proftpd/custom/ssh_host_rsa_key
 ssh-keygen -q -N ""  -t rsa -b 4096 -f conf/proftpd/custom/ssh_host_rsa_key
 test -d /var/tmp/.buildroot-ccache && rm -R /var/tmp/.buildroot-ccache
